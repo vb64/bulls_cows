3191529618efa45315e5a606d236d81f6a2e5c77
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2019-05-19 02:22:40 +0400

    game puzzle

--- source/alice.py
+++ source/alice.py
@@ -3,9 +3,28 @@
 Yandex.Alice skill
 """
 from models import Session
+from bull_cows import BullCows
 
+
+class Button:  # pylint: disable=too-few-public-methods,no-init,old-style-class
+    """
+    dialog buttons
+    """
+    CANCEL = "cancel"
+    HELP = "help"
+    AGAIN = "again"
+    EXIT = "exit"
+
+
+LABELS = {
+  Button.CANCEL: "Сдаюсь",
+  Button.HELP: "Помощь",
+  Button.AGAIN: "Да",
+  Button.EXIT: "Нет",
+}
 
 HELP_COMMANDS = ["помощь", "что ты умеешь"]
+CANCEL_COMMANDS = ["сдаюсь", "надоело", "не знаю"]
 
 HELP_MESSAGE = ''.join((
   "Этот навык умеет играть в логическую игру быки и коровы.", ' ',
@@ -22,6 +41,8 @@ START_MESSAGE = ''.join((
 ))
 
 PROMPT_MESSAGE = "Продиктуй число из четырех разных цифр."
+PROMPT_AGAIN = "Сыграем еще раз?"
+ERROR_MESSAGE = "Ошибка. Попробуйте начать новую игру."
 
 
 def remove_chars(text, chars):
@@ -50,18 +71,98 @@ def prompt(req, answer, prefix):
     if "screen" in req["meta"]["interfaces"]:
         answer['buttons'] = [
           {
-            "title": "Сдаюсь",
+            "title": LABELS[Button.CANCEL],
             "hide": True,
-            "payload": {"surrender": True},
+            "payload": {Button.CANCEL: True},
           },
           {
-            "title": "Помощь",
+            "title": LABELS[Button.HELP],
             "hide": False,
-            "payload": {"help": True},
+            "payload": {Button.HELP: True},
           },
         ]
 
     return answer
+
+
+def prompt_again(req, answer, prefix):
+    """
+    return prompt for Alice user
+    """
+    answer['text'] = prefix + PROMPT_AGAIN
+    if "screen" in req["meta"]["interfaces"]:
+        answer['buttons'] = [
+          {
+            "title": LABELS[Button.AGAIN],
+            "hide": True,
+            "payload": {Button.AGAIN: True},
+          },
+          {
+            "title": LABELS[Button.EXIT],
+            "hide": True,
+            "payload": {Button.EXIT: True},
+          },
+        ]
+
+    return answer
+
+
+def handle_button(req, answer, session):
+    """
+    button pressed
+    """
+    payload = req['request']['payload']
+
+    if Button.HELP in payload:
+        return prompt(req, answer, HELP_MESSAGE)
+
+    if Button.CANCEL in payload:
+        return finish(req, answer, session)
+
+    if Button.EXIT in payload:
+        return exit_session(answer, session)
+
+    if Button.AGAIN in payload:
+        return new_game(req, answer, "Отлично, сыграем еще раз.", session)
+
+    answer['text'] = ERROR_MESSAGE
+    answer['end_session'] = True
+
+    return answer
+
+
+def finish(req, answer, session):
+    """
+    user cancel current game
+    """
+    session.is_game_over = True
+    session.put()
+    prefix = "Сделано {} попыток угадать число {}\n\n".format(session.attempts_count, session.puzzle)
+
+    return prompt_again(req, answer, prefix)
+
+
+def exit_session(answer, session):
+    """
+    Alice user exit session
+    """
+    session.key.delete()
+    answer['text'] = "Возвращайся, поиграем еще."
+    answer['end_session'] = True
+
+    return answer
+
+
+def new_game(req, answer, prefix, session):
+    """
+    start new game in session
+    """
+    session.is_game_over = False
+    session.puzzle = BullCows().puzzle
+    session.attempts_count = 0
+    session.put()
+
+    return prompt(req, answer, prefix + START_MESSAGE)
 
 
 def new_session(req, answer):
@@ -88,10 +189,7 @@ def new_session(req, answer):
     if not session:
         session = Session(id=session_id)
 
-    session.attempts_count = 0
-    session.put()
-
-    return prompt(req, answer, prefix + START_MESSAGE)
+    return new_game(req, answer, prefix, session)
 
 
 def dialog(req):
@@ -102,5 +200,23 @@ def dialog(req):
 
     if req['session']['new']:
         return new_session(req, answer)
+
+    session_id = req['session']['session_id']
+    session = Session.get_by_id(session_id)
+    if not session:
+        # deleted session?
+        answer['text'] = ERROR_MESSAGE
+        answer['end_session'] = True
+        return answer
+
+    if req['request']['type'] == 'ButtonPressed':
+        return handle_button(req, answer, session)
+
+    text = normalize(req['request']['original_utterance'])
+    if text in HELP_COMMANDS:
+        return prompt(req, answer, HELP_MESSAGE)
+
+    if text in CANCEL_COMMANDS:
+        return finish(req, answer, session)
 
     return answer
--- source/models.py
+++ source/models.py
@@ -8,5 +8,7 @@ class Session(ndb.Model):  # pylint: disable=too-few-public-methods
     """
     key.id: alice session_id
     """
+    puzzle = ndb.TextProperty(default=None)
     last_attempt = ndb.DateTimeProperty(auto_now=True)
     attempts_count = ndb.IntegerProperty(default=None, indexed=False)
+    is_game_over = ndb.BooleanProperty(default=False, indexed=False)
