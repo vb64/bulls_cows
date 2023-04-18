de94239b1398af387a6efe07e43931ae0780293c
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-18 17:23:14 +0400

    Use test_helper_gae3 library (#20)

--- tests/test/__init__.py
+++ tests/test/__init__.py
@@ -1,33 +1,17 @@
 """Root class for testing."""
-# https://cloud.google.com/appengine/docs/standard/python3/reference/services/bundled/google/appengine/ext/testbed/Testbed
-from google.appengine.ext import testbed
 from tester_flask import TestFlask
+from test_helper_gae3 import TestGae3
 
 
-class TestCase(TestFlask):
+class TestCase(TestFlask, TestGae3):
     """Base class."""
 
     def setUp(self, app):
         """Use Flask app."""
         TestFlask.setUp(self, app)
-
-        self.gae_testbed = testbed.Testbed()
-        self.gae_testbed.activate()
-        # self.gae_testbed.setup_env()
-        self.gae_testbed.init_datastore_v3_stub()
-        self.gae_testbed.init_memcache_stub()
+        TestGae3.set_up(self)
 
     def tearDown(self):
         """Clear tests."""
-        self.gae_testbed.deactivate()
+        TestGae3.tear_down(self)
         super().tearDown()
-
-    def check_db_tables(self, db_state):
-        """Check record count for given GAE db tables."""
-        for table, count in db_state:
-            i = len(table.query().fetch(300, keys_only=True))
-            assert i == count, "{} items: {} must be {}".format(
-              table._get_kind(),  # pylint: disable=protected-access
-              i,
-              count
-            )
